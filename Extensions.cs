d7e61949c5d187d0b3f7d742734b22254e49fdfb
Oren (electricessence) <oren@electrified.net>
2020-07-16 12:08:52 -0700

    Sepearated from Open super lib.

--- Extensions.cs
+++ Extensions.cs
@@ -1,186 +1,185 @@
-﻿using System;
+﻿using Open.Threading;
+using System;
 using System.Data;
+using System.Diagnostics.Contracts;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
-using Open.Threading;
 
 
 namespace Open.Serialization
 {
-    public static class Extensions
-    {
+	public static class Extensions
+	{
+		private static void SerializeToInternal<T>(string path, T data, FileMode filemode = FileMode.OpenOrCreate)
+		{
+			if (path == null) throw new ArgumentNullException(nameof(path));
+			if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException("Cannot be whitespace or empty.", nameof(path));
+			Contract.EndContractBlock();
 
-        private static void SerializeToInternal<T>(string path, T data, FileMode filemode = FileMode.OpenOrCreate)
-        {
-            if (path == null) throw new ArgumentNullException("path");
-            if (String.IsNullOrWhiteSpace(path)) throw new ArgumentException("Cannot be whitespace or empty.", "path");
+			if (data == null)
+				return;
 
-            if (data == null)
-                return;
+			if (path.EndsWith(".xml", StringComparison.CurrentCultureIgnoreCase))
+			{
 
-            if (path.EndsWith(".xml", StringComparison.CurrentCultureIgnoreCase))
-            {
+				if (data is DataTable table)
+				{
 
-                DataTable table = data as DataTable;
-                if (table != null)
-                {
+					if (table.TableName == null)
+						table.TableName = path;
 
-                    if (table.TableName == null)
-                        table.TableName = path;
+					if (table.DataSet == null)
+						(new DataSet()).Tables.Add(table);
 
-                    if (table.DataSet == null)
-                        (new DataSet()).Tables.Add(table);
+					table.WriteXml(path, XmlWriteMode.WriteSchema);
+					return;
+				}
 
-                    table.WriteXml(path, XmlWriteMode.WriteSchema);
-                    return;
-                }
+				if (data is DataSet dataset)
+				{
+					dataset.WriteXml(path);
+					return;
+				}
 
-                DataSet dataset = data as DataSet;
-                if (dataset != null)
-                {
-                    dataset.WriteXml(path);
-                    return;
-                }
+				var formatter = new XmlSerializer(typeof(T));
+				using (var fs = new FileStream(path,
+					filemode, FileAccess.Write, FileShare.None))
+					formatter.Serialize(fs, data);
 
-                var formatter = new XmlSerializer(typeof(T));
-                using (var fs = new FileStream(path,
-                    filemode, FileAccess.Write, FileShare.None))
-                    formatter.Serialize(fs, data);
+			}
+			else
+			{
 
-            }
-            else
-            {
+				var formatter = new BinaryFormatter();
+				using (var fs = new FileStream(path,
+					filemode, FileAccess.Write, FileShare.None))
+					formatter.Serialize(fs, data);
+			}
+		}
 
-                var formatter = new BinaryFormatter();
-                using (var fs = new FileStream(path,
-                    filemode, FileAccess.Write, FileShare.None))
-                    formatter.Serialize(fs, data);
-            }
-        }
+		/// <summary>
+		/// Stores the data to disk at the specified location path.  Infers XML serialization if the file extension is XML.  Otherwise binary.
+		/// </summary>
+		/// <typeparam name="T">The data type.</typeparam>
+		/// <param name="path">The location to save to.</param>
+		/// <param name="data"></param>
+		public static void SerializeTo<T>(string path, T data)
+		{
+			if (path == null) throw new ArgumentNullException(nameof(path));
+			if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException("Cannot be whitespace or empty.", nameof(path));
+			Contract.EndContractBlock();
 
-        /// <summary>
-        /// Stores the data to disk at the specified location path.  Infers XML serialization if the file extension is XML.  Otherwise binary.
-        /// </summary>
-        /// <typeparam name="T">The data type.</typeparam>
-        /// <param name="path">The location to save to.</param>
-        /// <param name="data"></param>
-        public static void SerializeTo<T>(string path, T data)
-        {
-            if (path == null) throw new ArgumentNullException("path");
-            if (String.IsNullOrWhiteSpace(path)) throw new ArgumentException("Cannot be whitespace or empty.","path");
+			ThreadSafety.File.EnsureDirectory(path);
+			ThreadSafety.File.WriteTo(path, () => SerializeToInternal(path, data));
+		}
 
-            ThreadSafety.File.EnsureDirectory(path);
-            ThreadSafety.File.WriteTo(path, () => SerializeToInternal(path, data));
-        }
+		private static T DeserializeFromInternal<T>(string path, out DateTime? modified)
+		{
+			if (path == null) throw new ArgumentNullException(nameof(path));
+			if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException("Cannot be whitespace or empty.", nameof(path));
+			Contract.EndContractBlock();
 
-        private static T DeserializeFromInternal<T>(string path, out DateTime? modified)
-        {
-            if (path == null) throw new ArgumentNullException("path");
-            if (String.IsNullOrWhiteSpace(path)) throw new ArgumentException("Cannot be whitespace or empty.", "path");
+			if (!File.Exists(path))
+			{
+				modified = null;
+				return default;
+			}
 
-            if (!System.IO.File.Exists(path))
-            {
-                modified = null;
-                return default(T);
-            }
+			modified = File.GetLastWriteTime(path);
+			using (FileStream fs = ThreadSafety.File.Unsafe.GetFileStreamForRead(path))
+			{
+				if (fs.Length == 0)
+					return default;
 
-            modified = System.IO.File.GetLastWriteTime(path);
-            using (FileStream fs = ThreadSafety.File.Unsafe.GetFileStreamForRead(path))
-            {
-                if (fs.Length == 0)
-                    return default(T);
+				if (path.EndsWith(".xml", StringComparison.CurrentCultureIgnoreCase))
+				{
+					var type = typeof(T);
 
-                if (path.EndsWith(".xml", StringComparison.CurrentCultureIgnoreCase))
-                {
-                    var type = typeof(T);
+					if (type == typeof(DataTable))
+					{
+						var table = new DataTable();
+						table.ReadXml(path);
+						return (T)((object)table);
+					}
 
-                    if (type == typeof(DataTable))
-                    {
-                        var table = new DataTable();
-                        table.ReadXml(path);
-                        return (T)((object)table);
-                    }
+					if (type == typeof(DataSet))
+					{
+						var dataset = new DataSet();
+						dataset.ReadXml(path);
+						return (T)(object)dataset;
+					}
 
-                    if (type == typeof(DataSet))
-                    {
-                        var dataset = new DataSet();
-                        dataset.ReadXml(path);
-                        return (T)((object)dataset);
-                    }
+					var formatter = new XmlSerializer(typeof(T));
+					var result = formatter.Deserialize(fs);
+					return result == null ? default : (T)result;
+				}
+				else
+				{
+					var formatter = new BinaryFormatter();
+					return (T)formatter.Deserialize(fs);
+				}
+			}
+		}
 
-                    var formatter = new XmlSerializer(typeof(T));
-                    var result = formatter.Deserialize(fs);
-                    return result == null ? default(T) : (T)result;
-                }
-                else
-                {
-                    var formatter = new BinaryFormatter();
-                    return (T)formatter.Deserialize(fs);
-                }
-            }
-        }
+		public static T DeserializeFrom<T>(string path)
+		{
+			if (path == null) throw new ArgumentNullException(nameof(path));
+			if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException("Cannot be whitespace or empty.", nameof(path));
+			Contract.EndContractBlock();
 
-        public static T DeserializeFrom<T>(string path)
-        {
-            if (path == null) throw new ArgumentNullException("path");
-            if (String.IsNullOrWhiteSpace(path)) throw new ArgumentException("Cannot be whitespace or empty.", "path");
+			return DeserializeFrom<T>(path, out _);
+		}
 
-            DateTime? modified;
-            return DeserializeFrom<T>(path, out modified);
-        }
+		public static T DeserializeFrom<T>(string path, out DateTime? modified)
+		{
+			if (path == null) throw new ArgumentNullException(nameof(path));
+			if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException("Cannot be whitespace or empty.", nameof(path));
+			Contract.EndContractBlock();
 
-        public static T DeserializeFrom<T>(string path, out DateTime? modified)
-        {
-            if (path == null) throw new ArgumentNullException("path");
-            if (String.IsNullOrWhiteSpace(path)) throw new ArgumentException("Cannot be whitespace or empty.", "path");
+			if (!File.Exists(path))
+			{
+				modified = null;
+				return default;
+			}
 
-            if (!System.IO.File.Exists(path))
-            {
-                modified = null;
-                return default(T);
-            }
+			DateTime? writetime = null;
+			T result = ThreadSafety.File.ReadFrom(path, () =>
+			{
+				var r = DeserializeFromInternal<T>(path, out DateTime? wt);
+				writetime = wt;
+				return r;
+			});
 
-            DateTime? writetime = null;
-            T result = ThreadSafety.File.ReadFrom(path, () =>
-            {
-                DateTime? wt = null;
-                var r = DeserializeFromInternal<T>(path, out wt);
-                writetime = wt;
-                return r;
-            });
+			modified = writetime;
+			return result;
+		}
 
-            modified = writetime;
-            return result;
-        }
+		public static T DeserializeExisting<T>(string path, out DateTime? modified, Func<T> dataFactory)
+		{
+			if (path == null) throw new ArgumentNullException(nameof(path));
+			if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException("Cannot be whitespace or empty.", nameof(path));
+			Contract.EndContractBlock();
 
-        public static T DeserializeExisting<T>(string path, out DateTime? modified, Func<T> dataFactory)
-        {
-            if (path == null) throw new ArgumentNullException("path");
-            if (String.IsNullOrWhiteSpace(path)) throw new ArgumentException("Cannot be whitespace or empty.", "path");
+			T result = DeserializeFrom<T>(path, out DateTime? writetime);
+			if (!writetime.HasValue)
+			{
+				if (!ThreadSafety.File.WriteToIfNotExists(path, () =>
+				{
+					result = dataFactory();
+					SerializeToInternal(path, result);
+					writetime = DateTime.Now;
+				}))
+				{
+					result = DeserializeFrom<T>(path, out writetime);
+					if (!writetime.HasValue)
+						throw new Exception("Unable to retrive file for deserialization: " + path);
+				}
+			}
 
-            DateTime? writetime = null;
-            T result = DeserializeFrom<T>(path, out writetime);
-            if (!writetime.HasValue)
-            {
-                if (!ThreadSafety.File.WriteToIfNotExists(path, () =>
-                {
-                    result = dataFactory();
-                    SerializeToInternal(path, result);
-                    writetime = DateTime.Now;
-                }))
-                {
-                    result = DeserializeFrom<T>(path, out writetime);
-                    if (!writetime.HasValue)
-                        throw new Exception("Unable to retrive file for deserialization: " + path);
-                }
-            }
+			modified = writetime;
+			return result;
+		}
 
-            modified = writetime;
-            return result;
-        }
-
-    }
+	}
 }
-
-
--- Formatting.cs
+++ Formatting.cs
@@ -1,52 +1,53 @@
 ﻿using System;
 using System.Data;
+using System.Diagnostics.Contracts;
 
 namespace Open.Data
 {
-    
+
+	namespace Open.Data
+	{
+		public static class Formatting
+		{
+
+			/// <summary>
+			/// Shortcut for validating if a DataTable contains any rows.
+			/// </summary>
+			public static bool Any(this DataTable table)
+			{
+				if (table == null) throw new NullReferenceException();
+				return table.Rows.Count != 0;
+			}
 
 
-    namespace Open.Data
-    {
-        public static class Formatting
-        {
+			public static double GetDouble(this DataRow target, string columnName)
+			{
+				if (target == null) throw new NullReferenceException();
+				if (columnName == null) throw new ArgumentNullException(nameof(columnName));
+				Contract.EndContractBlock();
 
-            /// <summary>
-            /// Shortcut for validating if a DataTable contains any rows.
-            /// </summary>
-            public static bool Any(this DataTable table)
-            {
-                if (table == null) throw new NullReferenceException();
-                return table.Rows.Count != 0;
-            }
+				// Attempting to assure precision.
+				if (target.IsNull(columnName))
+					return double.NaN;
 
+				object value = target[columnName];
+				return double.Parse(value.ToString());
+			}
 
-            public static double GetDouble(this DataRow target, string columnName)
-            {
-                if (target == null) throw new NullReferenceException();
-                if (columnName == null) throw new ArgumentNullException("columnName");
+			public static float GetFloat(this DataRow target, string columnName)
+			{
+				if (target == null) throw new NullReferenceException();
+				if (columnName == null) throw new ArgumentNullException(nameof(columnName));
+				Contract.EndContractBlock();
 
-                // Attempting to assure precision.
-                if (target.IsNull(columnName))
-                    return double.NaN;
+				// Attempting to assure precision.
+				if (target.IsNull(columnName))
+					return float.NaN;
 
-                object value = target[columnName];
-                return double.Parse(value.ToString());
-            }
-
-            public static float GetFloat(this DataRow target, string columnName)
-            {
-                if (target == null) throw new NullReferenceException();
-                if (columnName == null) throw new ArgumentNullException("columnName");
-
-                // Attempting to assure precision.
-                if (target.IsNull(columnName))
-                    return float.NaN;
-
-                object value = target[columnName];
-                return float.Parse(value.ToString());
-            }
-        }
-    }
+				object value = target[columnName];
+				return float.Parse(value.ToString());
+			}
+		}
+	}
 
 }
